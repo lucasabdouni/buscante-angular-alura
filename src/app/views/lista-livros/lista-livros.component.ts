e5c6735dbba871839fab3e59e28a7d1b950423f6
lucasabdouni <lucasabdouni@hotmail.com>
2024-04-09 13:36:25 -0300

    feat: interface created, result filtered with rxjs and listed on the screen

--- src/app/views/lista-livros/lista-livros.component.ts
+++ src/app/views/lista-livros/lista-livros.component.ts
@@ -1,22 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Item, Livro } from 'src/app/models/interfaces';
+import { LivroVolumeInfo } from 'src/app/models/livroVolumeInfo';
 import { LivroService } from 'src/app/service/livro.service';
 
 @Component({
   selector: 'app-lista-livros',
   templateUrl: './lista-livros.component.html',
-  styleUrls: ['./lista-livros.component.css']
+  styleUrls: ['./lista-livros.component.css'],
 })
-export class ListaLivrosComponent {
+export class ListaLivrosComponent implements OnDestroy {
+  listaLivros!: Livro[];
+  campoBusca: string = '';
+  subscrition!: Subscription;
+  livro!: Livro;
 
-  listaLivros: any[] = [];
-  campoBusca: string = ''
-
-  constructor(private service: LivroService) { }
+  constructor(private service: LivroService) {}
 
   buscarLivros() {
-    this.service.buscar(this.campoBusca).subscribe({
-      next:(retornoAPI) => console.log(retornoAPI),
-      error:(error) => console.log(error)
-    })
+    this.subscrition = this.service.buscar(this.campoBusca).subscribe({
+      next: (items) => {
+        this.listaLivros = this.LivrosResultadoParaLivro(items);
+      },
+      error: (error) => console.log(error),
+    });
+  }
+
+  LivrosResultadoParaLivro(items: Item[]): LivroVolumeInfo[] {
+    return items.map((item) => {
+      return new LivroVolumeInfo(item);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscrition.unsubscribe();
   }
 }
